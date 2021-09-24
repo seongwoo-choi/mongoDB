1c09fd91da8e803df5d082c6a6d45608a56e0400
seongwoo_choi <how0326@gmail.com>
2021-09-25 00:27:11 +0900

    User Document(row) 생성

--- src/server.js
+++ src/server.js
@@ -4,8 +4,6 @@ const app = express();
 const { User } = require('./models/User');
 const { URI } = require('../mongo_db_uri');
 
-const users = [];
-
 const server = async () => {
 
   // console.log(URI)
@@ -18,19 +16,20 @@ const server = async () => {
 
     app.use(express.json());
 
-    app.get('/user', function(req, res) {
-      return res.send({ users: users });
+    app.get('/user', (req, res)=>{
+      return res.send()
     });
 
-    app.post('/user', function(req, res) {
-      console.log(req.body);
-      users.push({ name: req.body.name, age: req.body.age });
-      return res.send({ sucess: req.body });
+    app.post('/user', async (req, res) => {
+      // req.body 가 User 스키마의 key 값이 모두 동일하다고 가정한다.
+      const user = new User(req.body)
+      // DB에서 저장 완료가 되고나서 값이 리턴되게 하기 위해서 awit 을 사용
+      await user.save()
+      return res.send({ user });
     });
 
-    app.listen(3000, () => {
-      console.log('server listening on port 3000');
-    });
+    app.listen(3000, () => console.log('server listening on port 3000'))
+
   } catch (err) {
     console.log(err);
   }
