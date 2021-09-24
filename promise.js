45e6b7670e0f00bb6f262aa8582dc1cd5e9506b9
seongwoo <how0326@gmail.com>
2021-09-24 17:52:00 +0900

    async await
    - async await 는 promise 객체의 성공 실패의 값  .then .catch 를 사용하지 않고 편하게 사용할 수 있게 해준다.
    - try, catch 를 사용하여 .catch 를 사용 가능, error 도 쉽게 잡아줄 수 있다.

--- promise.js
+++ promise.js
@@ -7,7 +7,24 @@ const addSum = (a, b) => {
   });
 };
 
-addSum(10, 20)
-  .then((sum1) => addSum(sum1, 30))
-  .then((sum2) => console.log({ sum2 }))
-  .catch((error) => console.log({ error }));
+// addSum(10, 20)
+//   .then(sum1 => addSum(sum1, 40))
+//   .then(sum2 => addSum(sum2, 30))
+//   .then(sum3 => addSum(sum3, 40))
+//   .then(sum => console.log({ sum }))
+//   .catch(error => console.log({ error }));
+
+// async await 는 .then .catch 를 사용하지 않고 편하게 사용할 수 있게 해준다.
+// try, catch 를 사용하여 .catch 를 사용 가능, error 도 쉽게 잡아줄 수 있다.
+const totalSum = async () => {
+  try {
+    let sum = await addSum(10, 10);
+    let sum2 = await addSum(sum, 20);
+    let sum3 = await addSum(sum2, 30);
+    console.log({ sum, sum2 });
+  } catch (error) {
+    console.log({ error });
+  }
+};
+
+totalSum();
