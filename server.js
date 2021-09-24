2a5a8964ec37efba0fb23305dcb0bb56cb866d2e
seongwoo <how0326@gmail.com>
2021-09-24 16:55:51 +0900

    Express Middleware 사용

--- server.js
+++ server.js
@@ -3,13 +3,21 @@ const app = express();
 
 const users = [];
 
-app.get('/user', (req, res) => {
+app.use(express.json());
+
+app.post('/', (req, res)=>{
+  users.push({name: req.body.name, age: req.body.age})
+  return res.send(users)
+})
+
+app.get('/user', function(req, res) {
   return res.send({ users: users });
 });
 
-app.post('/user', (req, res) => {
-  uesrs.push({ name: 'how', age: 26 })
-  return res.send({ success: true });
+app.post('/user', function(req, res) {
+  console.log(req.body);
+  users.push({ name: req.body.name, age: req.body.age });
+  return res.send({ sucess: req.body });
 });
 
 app.listen(3000, () => {
