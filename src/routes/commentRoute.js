2ccb679fe065a6b1867366066edf5c0e79c09647
seongwoo-choi <how0326@naver.com>
2021-09-28 15:04:08 +0900

    Promise.all 사용
    - await 를 따로 사용하고 있을 경우 Promise.all 을 사용하면 병렬로 await 처리하기 때문에 성능이 올라간다. => response time 줄어든다.

--- src/routes/commentRoute.js
+++ src/routes/commentRoute.js
@@ -23,14 +23,21 @@ commentRouter.post('/', async (req, res) => {
     if (!isValidObjectId(userId)) return res.status(400).send({ err: 'userId is invalid' });
     if (typeof content !== 'string') return res.status(400).send({ err: 'content is requried' });
 
-    const blog = await Blog.findById(blogId);
-    const user = await User.findById(userId);
+    const [blog, user] = await Promise.all({
+      Blog.findById(blogId),
+      User.findById(userId),
+    });
+
+    // await 를 두 번 따로 사용하고 있는 데 Promise.all 을 사용하면 한번에 처리가 가능 => 병렬처리를 하기 때문에 성능이 오른다.
+    // const blog = await Blog.findById(blogId);
+    // const user = await User.findById(userId);
     if (!blog && !user) return res.status(400).send({ err: 'blog or user does not exist' });
 
     if (!blog.islive) return res.status(400).send({ err: 'blog is not avaliable' });
 
     // DB 에서 Comment 객체를 생성하여 node.js 에 생성
     const comment = new Comment({ content, user, blog });
+    await comment.save();
 
     return res.send({ comment });
   } catch (err) {
