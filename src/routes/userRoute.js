ecb8aac1ab2bbb08e937eebc64e682517b1212df
seongwoo-choi <how0326@naver.com>
2021-10-05 10:42:20 +0900

    arrayFilter 사용하여 userRoute 수정 (put 요청)
    - arrayFilters 를 이용하면 배열 객체의 값을 손쉽게 변경할 수 있다.
    - { arrayFilters: [{ 'comment.user': userId }] } 안의 값이 변경할 내용이며 comment 는 comments.$[comment].userFullName 의 $[comment] 배열값이다.

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -87,16 +87,7 @@ userRouter.put('/:userId', async (req, res) => {
                 .status(400)
                 .send({ error: 'firts and last name are string' });
 
-        // 이렇게 하는 이유? => age: age, name: name 으로 했을 경우엔 둘 중 하나의 값이 안들어왔을 경우 null 이 들어왔다고 처리를 하기 때문이다.
-        // let updateBody = {};
-        // if (age) updateBody.age = age;
-        // if (name) updateBody.name = name;
-
-        // key value 가 같아서 { $set: { age } } 로 간단하게 줄일 수 있다.
-        // filter => userId, update => { $set: { age: age } }, options => { new: true }
-        // const user = await User.findByIdAndUpdate(userId, updateBody, { new: true });
-
-        // 주석처리 된 위의 코드와 동일하다. 차이점은 노드에서 User 객체를 만들어서 노드에서 자체적으로 로직을 처리하고 그 후에 서버로 값을 보내는 것
+        // 노드에서 User 객체를 만들어서 노드에서 자체적으로 로직을 처리하고 그 후에 서버로 값을 보내는 것
         let user = await User.findById(userId);
         // 업데이트 되기 전 user
         console.log({ userBeforeEdit: user });
@@ -105,10 +96,19 @@ userRouter.put('/:userId', async (req, res) => {
             user.name = name;
             // Blog 스키마를 가져오고 필터링을 한다 => user 속성의 객체의 _id 가 userId 인 녀석들의 값을 가져오고
             // 그 중에서 user 객체의 name 속성을 name 으로 변경하는데 하나만 변경하는 것이 아닌 전부 변경한다.
-            await Blog.updateMany(
-                { 'user._id': userId },
-                { 'user.name': name },
-            );
+            // comments 속성의 배열값 안의 userFullName 속성의 값을 변경한다.
+            // arrayFilters 를 이용하면 배열 객체의 값을 손쉽게 변경할 수 있다.
+            // arrayFilters: [{}] 안의 값이 변경할 내용
+            await Promise.all([
+                Blog.updateMany({ 'user._id': userId }, { 'user.name': name }),
+                Blog.updateMany(
+                    {},
+                    {
+                        'comments.$[comment].userFullName': `${name.first} ${name.last}`,
+                    },
+                    { arrayFilters: [{ 'comment.user': userId }] },
+                ),
+            ]);
         }
         // 업데이트 된 user
         console.log({ userAfterEdit: user });
